274c295c95d346b44afcd9af3362d2e21a27dd73
adamnemecek <adamnemecek@gmail.com>
2021-10-18 18:19:53 -0700

    nitpicks

--- Sources/KoreMetal/Ext.swift
+++ Sources/KoreMetal/Ext.swift
@@ -2,21 +2,21 @@ import MetalKit
 
 extension MTLRenderCommandEncoder {
     // todo: should these be stride as opposed to size?
-    public func setVertexValue<T>(value: T, index: Int) {
+    public func setVertexValue<T>(_ value: T, index: Int) {
         var value = value
         self.setVertexBytes(&value, length: MemoryLayout<T>.size, index: index)
     }
 
-    public func setFragmentValue<T>(value: T, index: Int) {
+    public func setFragmentValue<T>(_ value: T, index: Int) {
         var value = value
         self.setFragmentBytes(&value, length: MemoryLayout<T>.size, index: index)
     }
 
-    public func setVertexArray<Element>(array: GPUArray<Element>, offset: Int, index: Int) {
+    public func setVertexArray<Element>(_ array: GPUArray<Element>, offset: Int, index: Int) {
         self.setVertexBuffer(array.raw.buffer, offset: offset, index: index)
     }
 
-    public func setFragmentArray<Element>(array: GPUArray<Element>, offset: Int, index: Int) {
+    public func setFragmentArray<Element>(_ array: GPUArray<Element>, offset: Int, index: Int) {
         self.setFragmentBuffer(array.raw.buffer, offset: offset, index: index)
     }
 }
