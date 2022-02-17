b12ca69474a92cc9eb7fb4b868b94da11de20fab
adamnemecek <adamnemecek@gmail.com>
2022-02-17 12:22:50 -0800

    stuff

--- Sources/KoreMetal/Ext.swift
+++ Sources/KoreMetal/Ext.swift
@@ -2,35 +2,66 @@ import MetalKit
 
 extension MTLRenderCommandEncoder {
     // todo: should these be stride as opposed to size?
-    public func setVertexValue<T>(_ value: T, index: Int) {
+    public func setVertexValue<T>(
+        _ value: T,
+        index: Int
+    ) {
         var value = value
         self.setVertexBytes(&value, length: MemoryLayout<T>.size, index: index)
     }
 
-    public func setFragmentValue<T>(_ value: T, index: Int) {
+    public func setFragmentValue<T>(
+        _ value: T,
+        index: Int
+    ) {
         var value = value
         self.setFragmentBytes(&value, length: MemoryLayout<T>.size, index: index)
     }
 
-    public func setVertexArray<Element>(_ array: GPUArray<Element>, offset: Int, index: Int) {
+    public func setVertexArray<Element>(
+        _ array: GPUArray<Element>,
+        offset: Int,
+        index: Int
+    ) {
         self.setVertexBuffer(array.raw.buffer, offset: offset, index: index)
     }
 
-    public func setFragmentArray<Element>(_ array: GPUArray<Element>, offset: Int, index: Int) {
+    public func setFragmentArray<Element>(
+        _ array: GPUArray<Element>,
+        offset: Int,
+        index: Int
+    ) {
         self.setFragmentBuffer(array.raw.buffer, offset: offset, index: index)
     }
 
-    public func setVertexUniforms<Element>(_ uniforms: GPUUniforms<Element>, index: Int) {
+    public func setVertexUniforms<Element>(
+        _ uniforms: GPUUniforms<Element>,
+        index: Int
+    ) {
         self.setVertexBuffer(uniforms.buffer, offset: 0, index: index)
     }
 
-    public func setFragmentUniforms<Element>(_ uniforms: GPUUniforms<Element>, index: Int) {
+    public func setFragmentUniforms<Element>(
+        _ uniforms: GPUUniforms<Element>,
+        index: Int
+    ) {
         self.setFragmentBuffer(uniforms.buffer, offset: 0, index: index)
     }
 }
 
 extension MTLComputeCommandEncoder {
-    public func setArray<Element>(_ array: GPUArray<Element>, offset: Int, index: Int) {
+    public func setArray<Element>(
+        _ array: GPUArray<Element>,
+        offset: Int,
+        index: Int
+    ) {
         self.setBuffer(array.raw.buffer, offset: offset, index: index)
+    }
+
+    public func setUniforms<Element>(
+        _ uniforms: GPUUniforms<Element>,
+        index: Int
+    ) {
+        self.setBuffer(uniforms.buffer, offset: 0, index: index)
     }
 }
