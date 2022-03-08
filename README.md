9b844b65caaec33885fee56694188df2189a52b6
adamnemecek <adamnemecek@gmail.com>
2022-03-07 19:44:08 -0800

    inline array

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # KoreMetal
+https://github.com/gonzoooooo/TodoApp/blob/6bc58b372107be0a05e23ca29e3c79dc09d9779a/Sources/TodoFeature/TodoListView.swift
+
 
 `GPUArray` which is a wrapper around `MTLBuffer`.
--- Sources/KoreMetal/ArrayRef.swift
+++ Sources/KoreMetal/ArrayRef.swift
@@ -1,4 +1,5 @@
 
+/// this is for perf comparisons
 public final class ArrayRef<Element> : Sequence, MutableCollection, RangeReplaceableCollection {
     var inner: ContiguousArray<Element>
 
@@ -34,5 +35,52 @@ public final class ArrayRef<Element> : Sequence, MutableCollection, RangeReplace
     public func replaceSubrange<C>(_ subrange: Range<Int>, with newElements: C) where C : Collection, Element == C.Element {
         self.inner.replaceSubrange(subrange, with: newElements)
     }
+}
 
+
+/// this is for perf comparisons
+public final class InlineArrayRef<Element> : Sequence, MutableCollection, RangeReplaceableCollection {
+    @usableFromInline
+    var inner: ContiguousArray<Element>
+
+
+    @inline(__always) @inlinable
+    public init() {
+        self.inner = []
+    }
+
+    @inline(__always) @inlinable
+    public var startIndex: Int {
+        self.inner.startIndex
+    }
+
+    @inline(__always) @inlinable
+    public var endIndex: Int {
+        self.inner.endIndex
+    }
+
+    @inline(__always) @inlinable
+    public subscript(index: Int) -> Element {
+        get {
+            self.inner[index]
+        }
+        set {
+            self.inner[index] = newValue
+        }
+    }
+
+    @inline(__always) @inlinable
+    public func index(after i: Int) -> Int {
+        i + 1
+    }
+
+    @inline(__always) @inlinable
+    public func append<S>(contentsOf newElements: S) where S : Sequence, Element == S.Element {
+        self.inner.append(contentsOf: newElements)
+    }
+
+    @inline(__always) @inlinable
+    public func replaceSubrange<C>(_ subrange: Range<Int>, with newElements: C) where C : Collection, Element == C.Element {
+        self.inner.replaceSubrange(subrange, with: newElements)
+    }
 }
