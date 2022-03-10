8501bc62415e8e72c7dffdb65ce6a6d46db25a2d
adamnemecek <adamnemecek@gmail.com>
2022-03-10 13:58:20 -0800

    drawrectangles

--- Sources/KoreMetal/MetalExt.swift
+++ Sources/KoreMetal/MetalExt.swift
@@ -2,12 +2,12 @@ import Metal
 
 extension MTLRenderCommandEncoder {
     @inline(__always) @inlinable
-    public func drawRectangles(count: Int) {
+    public func drawRectangles(instanceCount: Int) {
         self.drawPrimitives(
             type: .triangleStrip,
             vertexStart: 0,
             vertexCount: 4,
-            instanceCount: count
+            instanceCount: instanceCount
         )
     }
 }
