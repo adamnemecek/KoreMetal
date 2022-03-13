ffad44d008f65a389c212385c19ab2e390c29276
adamnemecek <adamnemecek@gmail.com>
2022-03-13 10:15:18 -0700

    indirect buffers

--- Sources/KoreMetal/MetalExt.swift
+++ Sources/KoreMetal/MetalExt.swift
@@ -12,6 +12,22 @@ extension MTLRenderCommandEncoder {
     }
 }
 
+extension MTLIndirectRenderCommand {
+    @inline(__always) @inlinable
+    public func drawRectangles(
+        instanceCount: Int,
+        baseInstance: Int
+    ) {
+        self.drawPrimitives(
+            .triangleStrip,
+            vertexStart: 0,
+            vertexCount: 4,
+            instanceCount: instanceCount,
+            baseInstance: baseInstance
+        )
+    }
+}
+
 extension MTLRenderCommandEncoder {
     // todo: should these be stride as opposed to size?
     @inline(__always) @inlinable
@@ -117,6 +133,87 @@ extension MTLComputeCommandEncoder {
             uniforms.buffer,
             offset: 0,
             index: index
+        )
+    }
+}
+
+extension MTLIndirectRenderCommand {
+//    @inline(__always) @inlinable
+//    public func setVertexValue<T>(
+//        _ value: T,
+//        index: Int
+//    ) {
+//        var value = value
+//        self.setVertexBytes(
+//            &value,
+//            length: MemoryLayout<T>.size,
+//            index: index
+//        )
+//    }
+//
+//    @inline(__always) @inlinable
+//    public func setFragmentValue<T>(
+//        _ value: T,
+//        index: Int
+//    ) {
+//        var value = value
+//        self.setFragmentBytes(
+//            &value,
+//            length: MemoryLayout<T>.size,
+//            index: index
+//        )
+//    }
+
+    ///
+    /// offset is in elements
+    ///
+    @inline(__always) @inlinable
+    public func setVertexArray<Element>(
+        _ array: GPUArray<Element>,
+        offset: Int,
+        at index: Int
+    ) {
+        self.setVertexBuffer(
+            array._raw._buffer,
+            offset: MemoryLayout<Element>.size * offset,
+            at: index
+        )
+    }
+
+    @inline(__always) @inlinable
+    public func setFragmentArray<Element>(
+        _ array: GPUArray<Element>,
+        offset: Int,
+        at index: Int
+    ) {
+        self.setFragmentBuffer(
+            array._raw._buffer,
+            offset: MemoryLayout<Element>.size * offset,
+            at: index
+        )
+    }
+
+    @inline(__always) @inlinable
+    public func setVertexUniforms<Element>(
+        _ uniforms: GPUUniforms<Element>,
+        at index: Int
+    ) {
+        self.setVertexBuffer(
+            uniforms.buffer,
+            offset: 0,
+            at: index
+        )
+    }
+
+    @inline(__always) @inlinable
+    public func setFragmentUniforms<Element>(
+        _ uniforms: GPUUniforms<Element>,
+        at index: Int
+    ) {
+        self.setFragmentBuffer(
+            uniforms.buffer,
+            offset: 0,
+            at: index
         )
     }
 }
